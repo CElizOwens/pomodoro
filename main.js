85cebb05a42e6eb1e83d0a49c6c237cb4e16537a
CElizOwens <owens.christina@gmail.com>
2023-03-15 18:27:06 -0700

    Added content to manifest.js, created createTimer in main.js

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
 // some sort of cache to store timer/ time timer was started
 // set body/display to null to hide the contents
 
-
 // const contents = document.getElementById('contents');
 // contents.parentNode.removeChild(contents);
 
@@ -11,14 +10,38 @@
 // check if website is not in the list of "allowed" websites
 // if it's not, redirect user to index file with timer
 let currentLocation = window.location.href;
-console.log(currentLocation)
+console.log(currentLocation);
 // array of "allowed" websites
 // alert the user of focus time remaining
-const allowedSites = ["https://developer.mozilla.org/en-US/", "https://www.codesmith.io/", "https://www.w3schools.com/", "https://www.codecademy.com/"]
+const allowedSites = [
+  'https://developer.mozilla.org/en-US/',
+  'https://www.codesmith.io/',
+  'https://www.w3schools.com/',
+  'https://www.codecademy.com/',
+];
 if (!allowedSites.includes(currentLocation)) {
-    window.location.href = "https://www.codesmith.io/"
+  window.location.href = 'https://www.codesmith.io/';
 }
+// declare function to create a timer based on amount of minutes passed in
+class createTimer {
+  //constructor
+  constructor(minutes) {
+    this.msLeft = minutes * 60;
+    this.timer = null;
+  }
+  //methods
+  start() {
+    this.timer = setInterval(() => this.msLeft--, 1000);
+  }
+  end() {
+    clearInterval(this.timer);
+  }
+}
+
+const studyTimer = new createTimer(1);
+// studyTimer.start();
+// setTimeout(() => studyTimer.end(), 4000);
 
 // set study parameters
 // set total timer for total time
-// 
+//
