6fb4ebad8fa49ded0469342cec6f72f28d5c0d63
Anne-lise Emig <anneliseemig@gmail.com>
2023-03-15 21:31:12 -0500

    added allowed links and domain names

--- main.js
+++ main.js
@@ -9,19 +9,32 @@
 // instead of changing the current website, we redirect to another website until the timer allows us to pass
 // check if website is not in the list of "allowed" websites
 // if it's not, redirect user to index file with timer
-let currentLocation = window.location.href;
-console.log(currentLocation);
+let currentLocation = window.location.hostname
 // array of "allowed" websites
 // alert the user of focus time remaining
 const allowedSites = [
-  'https://developer.mozilla.org/en-US/',
-  'https://www.codesmith.io/',
-  'https://www.w3schools.com/',
-  'https://www.codecademy.com/',
+  'developer.mozilla.org',
+  'www.codesmith.io',
+  'www.w3schools.com',
+  'www.codecademy.com',
 ];
+
+const allowedSitesLinks = [
+    'https://www.codesmith.io/',
+    'https://developer.mozilla.org/en-US/',
+    'https://www.w3schools.com/default.asp',
+    'https://www.codecademy.com/learn'
+]
+
 if (!allowedSites.includes(currentLocation)) {
-  window.location.href = 'https://www.codesmith.io/';
+  // find a random number between 0 and 3
+  let randomNum = Math.floor(Math.random() * 3)
+  window.location.href = allowedSitesLinks[randomNum];
+  // alert the user that they're supposed to be studying
+  alert('This site is junk. Let\'s get back to coding!')
+  // maybe add an alert that tells you how much time you have left?
 }
+
 // declare function to create a timer based on amount of minutes passed in
 class createTimer {
   //constructor
